20c5fd0c500b951bc35927660b828d345919fbf7
M. R. Hossain <rez182@gmail.com>
2025-01-19 02:11:22 -0500

    tabs instead of spaces

--- plex_lib_mon.py
+++ plex_lib_mon.py
@@ -11,12 +11,12 @@ inotify_obj = inotify.adapters.InotifyTree(watch_dir, mask=inotify.constants.IN_
 
 # Continuously monitor for events
 try:
-    for event in inotify_obj.event_gen(yield_nones=False):
-        (_, type_names, path, filename) = event
+	for event in inotify_obj.event_gen(yield_nones=False):
+		(_, type_names, path, filename) = event
 
-        print("{}\tSETFACL: {}/{}".format(time.strftime("[%Y-%m-%d | %I:%M:%S %p]"), path, filename))
+		print("{}\tSETFACL: {}/{}".format(time.strftime("[%Y-%m-%d | %I:%M:%S %p]"), path, filename))
 
-        # set file/directory permissions
+		# set file/directory permissions
 
 except KeyboardInterrupt:
-    print("\nMonitoring stopped.")
+	print("\nMonitoring stopped.")
