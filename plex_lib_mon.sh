a8a56dace335fc2d1abffde2ab19f97222ddb48c
M. R. Hossain <rez182@gmail.com>
2025-01-19 02:46:20 -0500

    plex library file/dir monitor in bash
    currently it is monitoring all file/dir creations of a monitored directory. SETFACL not implemented yet.

---
+++ plex_lib_mon.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+watch_dir="./testDir"
+
+inotifywait -r -m -e create "$watch_dir" | while read event; do
+	filename=$(echo "$event" | cut -d ' ' -f 3-) # -f 3- means fields from 3 to the end
+	filepath="$watch_dir/$filename"
+	echo "$(date '+[%Y-%m-%d | %I:%M:%S %p]')  SETFACL: $filepath"
+done
