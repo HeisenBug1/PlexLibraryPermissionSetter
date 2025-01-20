88b24ac27a87488d86da250d0e174a6ff8636b3c
M. R. Hossain <rez182@gmail.com>
2025-01-19 23:50:24 -0500

    setting permission for plex on file/dir

--- plex_lib_mon.sh
+++ plex_lib_mon.sh
@@ -6,4 +6,6 @@ inotifywait -r -m -e create "$watch_dir" | while read event; do
 	filename=$(echo "$event" | cut -d ' ' -f 3-) # -f 3- means fields from 3 to the end
 	filepath="$watch_dir/$filename"
 	echo "$(date '+[%Y-%m-%d | %I:%M:%S %p]')  SETFACL: $filepath"
+	setfacl -m u:plex:rwx "$filepath"
+	setfacl -m g:plex:rwx "$filepath"
 done
