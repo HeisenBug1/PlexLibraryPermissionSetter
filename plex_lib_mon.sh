f82785c4022e36f4654318c2c9f2057bb62bc623
M. R. Hossain <rez182@gmail.com>
2025-01-20 01:15:42 -0500

    BugFix: filepath includes subdirectories
    correctly extracting the full filepath before setting the ACL. Also checking if dependency `inotify-tools` is installed, else installing it.

--- plex_lib_mon.sh
+++ plex_lib_mon.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
-watch_dir="./testDir"
+# check if inotify-tools is installed. If not, install it
+if ! [ -x "$(command -v inotifywait)" ]; then
+	echo "$(date '+[%Y-%m-%d | %I:%M:%S %p]')  Plex Lib Monitor (BASH): inotify-tools is not installed. Installing..."
+	sudo apt update
+	sudo apt install -y inotify-tools
+fi
 
-inotifywait -r -m -e create "$watch_dir" | while read event; do
-	filename=$(echo "$event" | cut -d ' ' -f 3-) # -f 3- means fields from 3 to the end
-	filepath="$watch_dir/$filename"
-	echo "$(date '+[%Y-%m-%d | %I:%M:%S %p]')  SETFACL: $filepath"
+# check if path to watch directory is provided, if not, exit, else assign it to watch_dir
+if [ -z "$1" ]; then
+	echo "$(date '+[%Y-%m-%d | %I:%M:%S %p]')  Plex Lib Monitor (BASH): Please provide path to watch directory"
+	exit 1
+else
+	watch_dir="$1"
+fi
+
+inotifywait -r -m -e create "$watch_dir" | while read path action file; do
+	filepath="${path}${file}"
+	echo "$(date '+[%Y-%m-%d | %I:%M:%S %p]')  Plex Lib Monitor (BASH):  SETFACL: $filepath"
 	setfacl -m u:plex:rwx "$filepath"
 	setfacl -m g:plex:rwx "$filepath"
 done
